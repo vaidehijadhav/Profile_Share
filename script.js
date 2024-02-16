43d5899aa4993a72750c3169e8534c2d2ef85796
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-02-16 23:34:36 +0530

    Add open and close modal script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
+// Modal open function
+const openModal = () => {
+    console.log("Modal is open");
+    modal.classList.add("active");
+    overlay.classList.add("overlayactive");
+};
+
+const closeModal = () =>{
+    console.log("Modal is closed");
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayactive");
+};
